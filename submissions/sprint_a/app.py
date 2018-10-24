c25b54083225c768168b3c9ade617c4f0381efef
Matt Parsons <maparsons83@yahoo.com>
2018-10-24 15:17:59 -0400

    Added suggested changes to sprint-a

--- submissions/sprint_a/app.py
+++ submissions/sprint_a/app.py
@@ -4,9 +4,11 @@ from flask import jsonify
 
 @app.route('/')
 def generate_epithets():
+    """Serves a placeholder for future epithet generator"""
     return jsonify({'epithets': []})
 
 
 @app.route('/vocabulary')
 def vocabulary():
+    """Serves a placeholder for future insult vocabulary"""
     return jsonify({'vocabulary': {}})
