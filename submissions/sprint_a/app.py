5e5fb4faac3e4c7e0221bcf6f6134229a51d5d73
Matt Parsons <maparsons83@yahoo.com>
2018-10-24 15:32:54 -0400

    Addressed feedback from PR

--- submissions/sprint_a/app.py
+++ submissions/sprint_a/app.py
@@ -4,9 +4,11 @@ from flask import jsonify
 
 @app.route('/')
 def generate_epithets():
+    """Serves up placeholder for future insult generator"""
     return jsonify({'epithets': []})
 
 
 @app.route('/vocabulary')
 def vocabulary():
+    """Serves up a vocab list of insult words from a file"""
     return jsonify({'vocabulary': {}})
