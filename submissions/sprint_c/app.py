9e8152faa90fa96c45f3eb1adb5bb3cde856c296
Jake Berg <jacobvictorberg@gmail.com>
2018-10-29 16:56:21 -0400

    Finished sprint-c

--- submissions/sprint_c/app.py
+++ submissions/sprint_c/app.py
@@ -1,6 +1,7 @@
 from intitialize import app, jsonify
 from helpers import EpithetGenerator
 from helpers import Vocabulary
+import random
 
 
 @app.route('/')
@@ -16,6 +17,17 @@ def generate_epithet_by_amount(amount):
     word_one, word_two, word_three = EpithetGenerator.epithet()
     epithets = {}
     for i in range(int(amount)):
+        word_one, word_two, word_three = EpithetGenerator.epithet()
+        epithet = "Thou {} {} {}!".format(word_one, word_two, word_three)
+        epithets[i+1] = epithet
+    return jsonify(epithets)
+
+
+@app.route('/epithet/random')
+def generate_epithet_random():
+    word_one, word_two, word_three = EpithetGenerator.epithet()
+    epithets = {}
+    for i in range(random.randrange(20)):
         word_one, word_two, word_three = EpithetGenerator.epithet()
         epithet = "Thou {} {} {}!".format(word_one, word_two, word_three)
         epithets[i+1] = epithet
--- submissions/sprint_c/test_app.py
+++ submissions/sprint_c/test_app.py
@@ -5,17 +5,21 @@ from app import app
 
 class TestApp(TestCase):
 
+
     def create_app(self):
         return app
-
 
     def test_generate_epithet(self):
         res = self.client.get('/')
         self.assertIsInstance(res.json["epithets"].encode("utf-8"), str)
 
     def test_generate_epithet_by_amount(self):
-        res = self.client.get('/')
-        self.assertIsInstance(res.json["epithets"].encode("utf-8"), str)
+        res = self.client.get('/epithet/2')
+        self.assertEqual(len(res.json), 2)
+
+    def test_vocabulary(self):
+        res = self.client.get('/vocabulary')
+        self.assertIsInstance(res.json, dict)
 
 if __name__ == '__main__':
     unittest.main()
